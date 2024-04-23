2d047cbe745e36d0c7e56855f98691659d4b3180
Shuvradeb <shuvradeb.saha@ntb.no>
2024-04-23 16:21:36 +0600

    Color print text

---
+++ src/main/java/colorprint/PrintWithColor.java
@@ -0,0 +1,14 @@
+package colorprint;
+
+public class PrintWithColor {
+  public static void main(String[] args) {
+    System.out.println("\u001B[30mBlack text");
+    System.out.println("\u001B[31mRed text");
+    System.out.println("\u001B[32mGreen text");
+    System.out.println("\u001B[33mYellow text");
+    System.out.println("\u001B[34mBlue text");
+    System.out.println("\u001B[35mPurple text");
+    System.out.println("\u001B[36mCyan text");
+    System.out.println("\u001B[37mGray text");
+  }
+}
